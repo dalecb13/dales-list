009841a739b03edcd1d4c1948c4d737a39e5bebc
dalecb13 <dalecb13@gmail.com>
2025-10-01 15:33:59 +0200

    displayed initial product

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
+import ProductSummary from "@/components/product-summary";
+import summaryData from "@/data";
 import { Heading, Section, Text } from "@radix-ui/themes";
-import Image from "next/image";
 
 export default function Home() {
   return (
@@ -8,6 +9,19 @@ export default function Home() {
         <Section>
           <Heading as="h1">Dale&apos;s List</Heading>
           <Text>A food rating website</Text>
+        </Section>
+        <Section>
+          {
+            summaryData.map(datum => {
+              return <ProductSummary
+                key={datum.productName}
+                productName={datum.productName}
+                location={datum.location}
+                overallRating={datum.overallRating}
+                imageUrl={datum.imageUrl}
+              />;
+            })
+          }
         </Section>
       </main>
     </div>
