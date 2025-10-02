3cc94b70f0170a53b82e2b10614521ff65dfdfe9
dalecb13 <dalecb13@gmail.com>
2025-10-02 18:25:50 +0200

    factored out comon colors

---
+++ colors.ts
@@ -0,0 +1,2 @@
+export const GOLD = '#ffcf40';
+export const OFF_WHITE = '#F2F0EF';
--- components/star-rating.tsx
+++ components/star-rating.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Flex, Text } from '@radix-ui/themes';
+import { GOLD, OFF_WHITE } from "@/colors";
 
 type StarRatingProps = {
   rating: number
@@ -21,7 +22,7 @@ const StarRating: React.FC<StarRatingProps> = ({ rating }) => {
                 return <svg key={index} height="24" viewBox="0 0 24 24" width="24">
                   <path d="M0 0h24v24H0z" fill="none" />
                   <path
-                    fill="#ffcf40"
+                    fill={GOLD}
                     stroke="none"
                     strokeWidth="1.5"
                     d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"
@@ -35,10 +36,10 @@ const StarRating: React.FC<StarRatingProps> = ({ rating }) => {
                 return <svg key={index} height="24" viewBox="0 0 24 24" width="24">
                   <defs>
                     <linearGradient id="grad2">
-                      <stop offset="0%" stopColor="#ffcf40" />
-                      <stop offset={percentageFill} stopColor="#ffcf40" />
-                      <stop offset={percentageFill} stopColor="#F2F0EF" />
-                      <stop offset="100%" stopColor="#F2F0EF" />
+                      <stop offset="0%" stopColor={GOLD} />
+                      <stop offset={percentageFill} stopColor={GOLD} />
+                      <stop offset={percentageFill} stopColor={OFF_WHITE} />
+                      <stop offset="100%" stopColor={OFF_WHITE} />
                     </linearGradient>            
                   </defs>
                   <path d="M0 0h24v24H0z" fill="none" />
@@ -54,7 +55,7 @@ const StarRating: React.FC<StarRatingProps> = ({ rating }) => {
                 return <svg key={index} height="24" viewBox="0 0 24 24" width="24">
                   <path d="M0 0h24v24H0z" fill="none" />
                   <path
-                    fill="#F2F0EF"
+                    fill={OFF_WHITE}
                     stroke="none"
                     strokeWidth="1.5"
                     d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"
